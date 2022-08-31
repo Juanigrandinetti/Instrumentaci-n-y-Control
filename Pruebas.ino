481a05c0b95d5ffac4a51fcbe1f19573dd7e35ff
Juanigrandinetti <93537816+Juanigrandinetti@users.noreply.github.com>
2022-08-31 18:37:56 -0300

    Update Pruebas.ino

--- Pruebas.ino
+++ Pruebas.ino
@@ -1,138 +1,118 @@
-/* Suscribete al canal de Profe Misa
-   https://www.youtube.com/c/ProfeMisa 
-
-   Código para control de temperatura usando PID
-   Basado en 220V AC PID CONTROL - CODE, para mas detalles consultar ELECTRONOOBS en Español  https://youtu.be/o3Q56a3mvOg
-
-   Para este prototipo, el objetivo de control es mantener la temperatura del foco en un setpoint   
-   Variable de entrada = Potencia (0 - 100), Variable de salida = Temperatura (Temp Ambiente - 80 ºC)
-*/ 
-int Modo = 2;  // Modo de operacion 1: Modo manual (lazo abierto)
-               //                   2: Modo automatico (lazo cerrado)
-               
-// Parametros para analisis a lazo abierto
-float Potencia_1 = 0;     // valor inicial del cambio escalon. (min = 0)
-float Potencia_2 = 0;   // Valor final del cambio escalon. (max = 100)
-
-// Parametros para analisis a lazo cerrado
-float Setpoint = 46;       // Celsius
-
-// Constantes de PID
-float Kc = 1; float Tao_I = 110;
-
-int Tiempo0 = 15000;     // Retardo (en milisegundos) para ejecutar cambio escalon cuando se encuentra 
-                         //a lazo abierto o cambio en el septpoint a lazo cerrado 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////     NO TOCAR      //////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////     NO TOCAR      //////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////     NO TOCAR      //////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int A = 0;           // Pin A0 de entrada analogica para sensor LM35 (Variable de salida)
-float Potencia = 0;  // Potencia inicial enviada al dimmer en rango de 0 a 100 (Variable de entrada)
-
-//  Declaracion de variables
-int pin_disparo = 3;
-int pin_cruce_cero = 8;
-
-int last_CH1_state = 0;
-int detectado = 0;
-int valor=0;
-
-unsigned long Tiempo_previo = 0; 
-unsigned long Tiempo_actual = 0;
-int Read_Delay = 1000;     // Periodo de muestreo en milisegundos
-int Temperatura = 0;       // Celsius
-float sp = 0;    
-
-// Variables para PID
-float PID_error = 0;
-float previous_error = 0;
-float PID_value = 0;
-float Error_INT = 0;
-
+/* 
+ *  El modo de operación permite elegir si el sistema será de lazo abierto o cerrado, es decir, manual o automático, respectivamente.
+ *  Modo = 1: lazo abierto -> modo manual.
+ *  Modo = 2: lazo cerrado -> modo automático.
+*/
+int Modo = 2; 
+/*
+ * Definimos el rango de valores de potencia que podrá emitir el foco. En este caso, puede variar entre 100 valores distintos.
+ */
+float potencia_min = 0;
+float potencia_max = 0;
+/*
+ * SetPoint (sp) se usa para elegir la temperatura deseada.
+ * La temperatura final será el sp más la temperatura ambiente donde se esté realizando el sensado.
+*/
+float SetPoint = 48;
+/*Parámetros PI*/
+float Kc = 5, Ti = 5;
+/*Retardo en milisegundos desde que se compila el programa hasta que la señal de entrada se pone en alto*/
+int Tiempo0 = 15000;
+/*PINES
+ * LM35: A0 -> Sensor de temperatura.
+ * PWM: 3   ->Señal que regula la potencia que emite el foco.
+ * ZC: 8    ->Señal que envía el módulo dimmer cada vez que la señal de AC cruza por el origen de coordenadas.
+*/
+const int LM35 = A0;
+const int PWM = 3;
+const int ZC = 8;
+/*Señal de entrada. Es la potencia enviada al foco.*/
+float Potencia = 0;
+/*Variables usadas para la rutina de Servicio de Interrupción.*/
+int estado = 0, ZC_flag = 0;
+/*Variable usada para almacenar el mapeo de los valores de potencia que emite el foco.*/
+int Potencia_map = 0;
+/*Variables de tiempo usadas para verificar si finalizó el retardo establecido por "Tiempo0".*/
+unsigned long Tiempo_previo = 0, Tiempo_actual = 0;
+/*Frecuencia de muestreo en milisegundos.*/
+int fm = 1000;
+/*Variable utilizada para almacenar los valores sensados por el LM35.*/
+int Temperatura_LM35 = 0;
+float sp = 0;
+/*ERROR
+ * error: diferencia entre la señal de entrada y la señal realimentada.
+ * error_i: error integral.
+ * error_total: suma del error proporcional más el error integral.
+*/
+float error = 0, error_i = 0, error_total = 0;
 void setup() {
   Serial.begin(9600);
-  //Definicion de pines
-  pinMode (pin_disparo,OUTPUT); 
-  pinMode (pin_cruce_cero,INPUT);
-  PCICR |= B00000001;                                                     
-  PCMSK0 |= B00000001;  
+  pinMode(PWM, OUTPUT);
+  pinMode(ZC, INPUT);
+  /*Habilitamos las interrupciones del puerto B.*/
+  PCICR |= B00000001;
+  /*Habilitamos el pin 8 ó D0 para interrupciones.*/
+  PCMSK0 |= B00000001;
 }
 
 void loop() {
-  
-    Tiempo_actual = millis(); // Tiempo Actual    
-     
-    valor = map(Potencia,0,100,0,2000);
-    if (detectado)
+  Tiempo_actual = millis();
+  Potencia_map = map(Potencia, 0, 100, 0, 2000);
+  if(ZC_flag)
+  {
+    delayMicroseconds(Potencia_map);
+    digitalWrite(PWM, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(PWM, LOW);
+    ZC_flag = 0;
+    /*Verificamos que haya pasado 1 segundo para así, poder tomar una muestra.*/
+    if(Tiempo_actual - Tiempo_previo >= fm)
     {
-      delayMicroseconds(valor);
-      digitalWrite(3,HIGH);
-      delayMicroseconds(100);
-      digitalWrite(3,LOW);
-      detectado=0;
-
-    if(Tiempo_actual - Tiempo_previo >= Read_Delay){
-       Tiempo_previo += Read_Delay;                
-    
-    Temperatura = 100.0*(5.0*analogRead(A)/1024.0 - .5);         //Lectura del sensor LM35
-
-    if(Modo == 1){
-      // Modo manual (lazo abierto)
-      if(Tiempo_actual <= Tiempo0) {
-        Potencia = Potencia_1;
+      Tiempo_previo += fm;
+      Temperatura_LM35 = 100.0*(5.0*analogRead(LM35)/1024.0 - .5);
+      if(Modo == 1)
+      {
+        if(Tiempo_actual <= Tiempo0) Potencia = potencia_min;
+        else if(Tiempo_actual >= Tiempo0) Potencia = potencia_max;
       }
-      else if(Tiempo_actual >= Tiempo0) {
-        Potencia = Potencia_2;
-      } 
-      
-    }
-    else if(Modo == 2){
-    // Modo automatico (lazo cerrado)
-    if(Tiempo_actual <= Tiempo0) {
-       PID_value = 0;
-    }
-    else if (Tiempo_actual >= Tiempo0){
-      PID_error = Setpoint - Temperatura;                   //Calculo del error    
-      Error_INT = Error_INT + PID_error*(1000/Read_Delay);  //Calculo de la integral del error
-      PID_value = Kc*(PID_error + (1/Tao_I)*Error_INT);     //Calculo de la salida del controlador PI
-
-      sp = Setpoint;
-      
-    }
-    // Limite de salida del controlador
-    if(PID_value < 0)
-    {      PID_value = 0;       }
-    if(PID_value > 100)
-    {      PID_value = 100;    }
-
-    Potencia = PID_value;   //Asignacion a la entrada de la planta.
-    }    
-    
-        Serial.print(Potencia);
-        Serial.print(" ");
-        Serial.print(Temperatura);
-        Serial.print(" ");
-        Serial.println(sp);    
-    }
-    
-    } 
-}
-
-//This is the interruption routine
-//----------------------------------------------
-
-ISR(PCINT0_vect){
-  /////////////////////////////////////               //Input from optocoupler
-  if(PINB & B00000001){                               //We make an AND with the pin state register, We verify if pin 8 is HIGH???
-    if(last_CH1_state == 0){                          //If the last state was 0, then we have a state change...
-      detectado=1;                                    //We haev detected a state change!
+      else if(Modo == 2)
+      {
+        if(Tiempo_actual <= Tiempo0) error_total = 0;
+        else if(Tiempo_actual >= Tiempo0)
+        {
+          error = SetPoint - Temperatura_LM35;
+          error_i = error_i + error*(1000/fm);
+          error_total = Kc*(error + (1/Ti)*error_i);
+        }
+        /*Valores límites para la salida del controlador.*/
+        if(error_total < 0) error_total = 0;
+        if(error_total > 100) error_total = 100;
+        /*Asignamos el error a la entrada del actuador para que implemente la acción correctora que corresponda.*/
+        Potencia = error_total;
+        sp = SetPoint;
+      }
+      Serial.print(Potencia);
+      Serial.print(" ");
+      Serial.print(Temperatura_LM35);
+      Serial.print(" ");
+      Serial.println(sp);
     }
   }
-  else if(last_CH1_state == 1){                       //If pin 8 is LOW and the last state was HIGH then we have a state change      
-    detectado=1;                                      //We haev detected a state change!
-    last_CH1_state = 0;                               //Store the current state into the last state for the next loop
+}
+/*Rutina de Servicio de Interrupción*/
+ISR(PCINT0_vect)
+{
+  /*Verificamos que el puerto B y el pin 8 ó D0 tengan las interrupciones habilitadas.*/
+  if(PINB & B00000001)
+  {
+    if(estado == 0) 
+    {
+      ZC_flag = 1;
     }
+  }
+  else if(estado == 1) 
+  {
+    ZC_flag = 1;
+    estado = 0;
+  }
 }
